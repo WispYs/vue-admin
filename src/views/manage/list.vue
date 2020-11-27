74675d1e59b3f7bee4974c0f1df4a36f63b4bfa0
wisper <wisper927@163.com>
2020-11-27 16:45:36 +0800

    feat: export table to excel

--- src/views/manage/list.vue
+++ src/views/manage/list.vue
@@ -132,6 +132,11 @@ import { fetchList } from '@/api/project'
 // import mockData from '@/mock/mock-data'
 import Pagination from '@/components/Pagination'
 import ListFilter from './components/ListFilter'
+
+// 导出 Excel 表格
+import FileSaver from 'file-saver'
+import XLSX from 'xlsx'
+
 export default {
   components: { Pagination, ListFilter },
   data() {
@@ -159,6 +164,31 @@ export default {
     })
   },
   methods: {
+    // 定义导出Excel表格事件
+    exportExcel() {
+      /* 从表生成工作簿对象 */
+      var wb = XLSX.utils.table_to_book(this.$refs.table.$el)
+      /* 获取二进制字符串作为输出 */
+      const wbout = XLSX.write(wb, {
+        bookType: 'xlsx',
+        bookSST: true,
+        type: 'array'
+      })
+      try {
+        FileSaver.saveAs(
+          // Blob 对象表示一个不可变、原始数据的类文件对象。
+          // Blob 表示的不一定是JavaScript原生格式的数据。
+          // File 接口基于Blob，继承了 blob 的功能并将其扩展使其支持用户系统上的文件。
+          // 返回一个新创建的 Blob 对象，其内容由参数中给定的数组串联组成。
+          new Blob([wbout], { type: 'application/octet-stream' }),
+          // 设置导出文件名称
+          'data.xlsx'
+        )
+      } catch (e) {
+        if (typeof console !== 'undefined') console.log(e, wbout)
+      }
+      return wbout
+    },
     __getList() {
       this.listLoading = true
       fetchList(this.listQuery).then(response => {

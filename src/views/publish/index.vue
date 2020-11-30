27146535fb8fa593740b4a2a6fc69042ffdf2294
wisper <wisper927@163.com>
2020-11-30 14:28:57 +0800

    feat: add editor plugin

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,15 +1,35 @@
 <template>
-  <div class="person-container">
-    person
+  <div class="publish-container">
+    <h2 class="publish-container__title">发布文章</h2>
+    <tinymce v-model="content" :height="300" />
   </div>
 </template>
 
 <script>
+import Tinymce from '@/components/Tinymce'
 export default {
-
+  components: { Tinymce },
+  data() {
+    return {
+      content:
+      `<h1 style="text-align: center;">欢迎上海XXX公司领导前来参观</h1>
+      <p style="text-align: center; font-size: 15px;">XXXXXXXXXXXXXXXXXXXXXXX</p>
+      <p>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
+      <p>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
+      <p>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
+      <p>XXXXXXXXXXXXXXXXXXXXXXXXX</p>
+      <p style="text-align: right; font-size: 15px;">2020-12-13</p>
+      `
+    }
+  }
 }
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+  .publish-container{
+    padding: 30px;
+    &__title {
+      margin-bottom: 30px;
+    }
+  }
 </style>

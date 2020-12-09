1dfbc730f920bb1ce9e1d08c7f804d03a0cffcf3
wisper <wisper927@163.com>
2020-12-09 17:34:28 +0800

    feat: change error tips

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -55,21 +55,21 @@ export default {
   data() {
     const validateUsername = (rule, value, callback) => {
       if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
+        callback(new Error('请输入正确的用户名'))
       } else {
         callback()
       }
     }
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+        callback(new Error('请输入正确的密码'))
       } else {
         callback()
       }
     }
     return {
       loginForm: {
-        username: 'admin',
+        username: 'ld-admin',
         password: '111111'
       },
       loginRules: {

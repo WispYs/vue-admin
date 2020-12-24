ef137bf86e39c03df442a31fb3a2755382e4bc4d
wisper <wisper927@163.com>
2020-12-24 14:41:14 +0800

    feat: change url address

--- src/api/url.js
+++ src/api/url.js
@@ -1,4 +1,5 @@
 // const baseUrl = 'https://mock.yonyoucloud.com/mock/16292/vue-admin'
-const baseUrl = 'http://192.168.50.104:9006'
+// const baseUrl = 'http://192.168.50.104:9006'
+const baseUrl = 'http://192.168.1.79:9006'
 
 export default baseUrl
